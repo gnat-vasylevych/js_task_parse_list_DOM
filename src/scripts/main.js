8b887ca8a76fadd41a4f3f331f0e3c573a336f3d
Vlad <vladyslav.vasylevych@gmail.com>
2024-12-10 20:05:57 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
 // write code here
+function main() {
+  const doc = document.documentElement;
+  const list = doc.querySelector('ul');
+
+  const children = Array.from(list.children);
+
+  children.sort(sortFunc);
+
+  for (const obj of children) {
+    list.appendChild(obj);
+  }
+}
+
+function sortFunc(a, b) {
+  const aSal = formatSalary(a.dataset.salary);
+  const bSal = formatSalary(b.dataset.salary);
+
+  return bSal - aSal;
+}
+
+function formatSalary(salary) {
+  const copy = salary.slice(1);
+
+  return +copy.split(',').join('');
+}
+
+main();
